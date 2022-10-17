0c2cf437cb701a73c0d5b2c656778737ffef91d3
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-17 20:33:24 +0530

    client configured

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import { HeroBanner, Footer, FooterBanner } from "../components";
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many varient</p>
       </div>
-      footer
+      <div className="products-container">{["product1", "product2", "product3", "product4"].map((product) => product)}</div>
+      <FooterBanner />
+      <Footer />
     </>
   );
 };
