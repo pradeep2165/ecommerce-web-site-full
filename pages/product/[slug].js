379fd3f2d5d6b46456de4f24d390f81b292db823
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-18 10:13:05 +0530

    image formated like carousel image

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,15 +5,20 @@ import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="product image" />
+            <img src={urlFor(image && image[index])} alt="product image" className="product-detail-image" />
           </div>
-          <div>{}</div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img key={i} src={urlFor(item)} className={i === index ? "small-image selected-image" : "small-image"} onMouseEnter={() => setIndex(i)} />
+            ))}
+          </div>
         </div>
         <div className="product-detail-desc">
           <h1>{name}</h1>
@@ -44,7 +49,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className="buttons">
             <button type="button" className="add-to-cart">
-              Add to Carth
+              Add to Cart
             </button>
             <button type="button" className="buy-now">
               Buy Now
